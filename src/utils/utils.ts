11a34cbc5bd84d42de18f87975cd73b0b4a56499
ZeeyahOke <f.oke@alustudent.com>
2025-07-17 15:09:44 +0200

    feat: add generateId utility function

---
+++ src/utils/utils.ts
@@ -0,0 +1,11 @@
+export function generateId() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  
+  for (let i = 0; i < 7; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  
+  return result;
+}
