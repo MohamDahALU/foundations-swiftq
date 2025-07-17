d1b3a2bd61fe099aead03144903adea86da1dd76
Joshualu <j.moses@alustudent.com>
2025-07-17 14:27:15 +0200

    feat: add custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#D5F279"
+        "primary": "#D5F279",
+        "primary-sat": "#C3FF0380"
+      },
+      fontSize: {
+        "2xs": ".7rem"
+      },
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' }
+        },
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        'slide-up': 'slide-up 0.3s ease-out forwards',
+        'fade-in': 'fade-in 0.2s ease-out forwards'
       }
     },
   },
   plugins: [],
 }
-
