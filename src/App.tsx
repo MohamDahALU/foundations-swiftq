d8c13dba4f77bb5fa5a3087de570758c74a1cdca
MohamDahALU <m.dahab@alustudent.com>
2025-07-23 15:13:34 +0200

    feat: add functionality to hostQueues and joinQueue & wrap protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,83 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import JoinQueue from "./pages/JoinQueue";
-import Login from "./pages/Login";
-import CreateQueue from "./pages/CreateQueue";
-import HostQueues from "./pages/HostQueues";
-import Signup from "./pages/Signup";
-import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ErrorPage from "./components/ErrorPage";
+
+import { HostQueueLoader } from "./pages/host-queues/loader";
+import Layout from "./components/Layout";
+import CustomerLayout from "./components/CustomerLayout";
+import CreateQueue from "./pages/CreateQueue";
+import HostQueues from "./pages/host-queues/HostQueues";
+import Home from "./pages/Home";
+import JoinQueue from "./pages/join-queue/JoinQueue";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { JoinQueueLoader } from "./pages/join-queue/loader";
+
 
 const router = createBrowserRouter([
   {
+    errorElement: <ErrorPage />,
     element: <Layout />,
-    errorElement: <h1>Error</h1>,
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <Home />
       },
       // Host Views
       {
-        path: "/create",
-        element: <CreateQueue />,
+        element: <CustomerLayout />,
+        children: [
+          {
+            path: "/join/:queueId",
+            element: (
+              <JoinQueue />
+            ),
+            loader: JoinQueueLoader
+          },
+          {
+            path: "/create",
+            element: (
+              <ProtectedRoute>
+                <CreateQueue />
+              </ProtectedRoute>
+            )
+          }
+        ]
+
       },
       {
         path: "/my-queues",
-        element: <HostQueues />,
-      },
-      {
-        path: "/my-queues/:queueId",
-        element: <h1>HostQueueDetails</h1>,
-      },
-      {
-        path: "/qr/:queueId",
-        element: <h1>QR</h1>,
+        element: (
+          <ProtectedRoute>
+            <HostQueues />
+          </ProtectedRoute>
+        ),
+        loader: HostQueueLoader
       },
 
-      // Customer Views
-      {
-        path: "/join/:queueId",
-        element: <JoinQueue />,
-      },
-      {
-        path: "/queue/:queueId/customer/:customerId",
-        element: <h1>CustomerView</h1>,
-      },
 
       // Auth Views
       {
         path: "/login",
-        element: <Login />,
+        element: <Login />
       },
       {
         path: "/register",
-        element: <Signup />,
+        element:
+          <Signup />
       },
-    ],
-  },
+    ]
+  }
 ]);
 function App() {
-  return <AuthProvider>
-    <RouterProvider router={router} />;
-  </AuthProvider>
+
+
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/host-queues/loader.ts
@@ -0,0 +1,27 @@
+// src/pages/my-queues/loader.ts
+import { requireAuth } from "../../firebase/authLoaders";
+import { getHostQueues, getQueueCustomers } from "../../firebase/services/queues";
+
+export async function HostQueueLoader() {
+  try {
+    // This will redirect to login if not authenticated
+    const huh = await requireAuth();
+    console.log(huh)
+    
+    // If we get here, user is authenticated
+    const data = await getHostQueues();
+    const queues = await Promise.all(data.map(async (qu) => ({
+      ...qu,
+      count: (await getQueueCustomers(qu.id)).length
+    })));
+    
+    return {queues};
+  } catch (err: unknown) {
+    // If this is a redirect response from requireAuth, pass it through
+    if (err instanceof Response && err.status === 302) {
+      throw err;
+    }
+    
+    throw new Response("Failed To Fetch Queues", { status: 500 });
+  }
+}
