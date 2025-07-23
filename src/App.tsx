e345d7ac61450617d18f9d5879d16c4a02b23dd9
MohamDahALU <m.dahab@alustudent.com>
2025-07-23 15:21:12 +0200

    feat: implement HostQueueDetails with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { JoinQueueLoader } from "./pages/join-queue/loader";
 import CustomerView from "./CustomerView";
+import HostQueueDetails from "./HostQueueDetails";
 
 
 const router = createBrowserRouter([
@@ -61,6 +62,14 @@ const router = createBrowserRouter([
           </ProtectedRoute>
         ),
         loader: HostQueueLoader
+      },
+      {
+        path: "/my-queues/:queueId",
+        element: (
+          <ProtectedRoute>
+            <HostQueueDetails />
+          </ProtectedRoute>
+        ),
       },
 
 
